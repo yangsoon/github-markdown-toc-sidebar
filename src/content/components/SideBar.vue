a90d9a4de8b611ec49da59f40cc877af6862e5f5
yang <yangsoonlx@gmail.com>
2019-10-30 21:51:22 +0800

    :sparkles: add toc to md file

--- src/content/components/SideBar.vue
+++ src/content/components/SideBar.vue
@@ -1,174 +1,59 @@
 <template>
-    <div>
-        <Button type="primary" @click="open">打开/关闭</Button>
-        <Drawer v-model="value" :draggable="true" :mask="false">
-            <Tree :data="data5" :render="renderContent"></Tree>
+    <div class="">
+        <Drawer v-model="value" :draggable="true" :mask="false" :scrollable="true">
+            <div class="toc-header">
+
+            </div>
+            <div class="toc-content-tree">
+               <Tree :data="tree" @on-select-change="navigation"></Tree>
+            </div>
         </Drawer>
     </div>
 </template>
 
 <script>
-    // import {Drawer, Tree} from "view-design"
     export default {
         name: "SideBar",
-        // components: {
-        //     Drawer,
-        //     Tree
-        // },
+        props: {
+          tree: Array
+        },
         data () {
             return {
                 value: true,
-               data5: [
-                    {
-                        title: 'parent 1',
-                        expand: true,
-                        render: (h, { root, node, data }) => {
-                            return h('span', {
-                                style: {
-                                    display: 'inline-block',
-                                    width: '100%'
-                                }
-                            }, [
-                                h('span', [
-                                    h('Icon', {
-                                        props: {
-                                            type: 'ios-folder-outline'
-                                        },
-                                        style: {
-                                            marginRight: '8px'
-                                        }
-                                    }),
-                                    h('span', data.title)
-                                ]),
-                                h('span', {
-                                    style: {
-                                        display: 'inline-block',
-                                        float: 'right',
-                                        marginRight: '32px'
-                                    }
-                                }, [
-                                    h('Button', {
-                                        props: Object.assign({}, this.buttonProps, {
-                                            icon: 'ios-add',
-                                            type: 'primary'
-                                        }),
-                                        style: {
-                                            width: '64px'
-                                        },
-                                        on: {
-                                            click: () => { this.append(data) }
-                                        }
-                                    })
-                                ])
-                            ]);
-                        },
-                        children: [
-                            {
-                                title: 'child 1-1',
-                                expand: true,
-                                children: [
-                                    {
-                                        title: 'leaf 1-1-1',
-                                        expand: true
-                                    },
-                                    {
-                                        title: 'leaf 1-1-2',
-                                        expand: true
-                                    }
-                                ]
-                            },
-                            {
-                                title: 'child 1-2',
-                                expand: true,
-                                children: [
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        expand: true
-                                    },
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        expand: true
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
-                buttonProps: {
-                    type: 'default',
-                    size: 'small',
-                }
             }
         },
         methods: {
-            open(){
-                this.value = !this.value
-            },
-            renderContent (h, { root, node, data }) {
-                return h('span', {
-                    style: {
-                        display: 'inline-block',
-                        width: '100%'
-                    }
-                }, [
-                    h('span', [
-                        h('Icon', {
-                            props: {
-                                type: 'ios-paper-outline'
-                            },
-                            style: {
-                                marginRight: '8px'
-                            }
-                        }),
-                        h('span', data.title)
-                    ]),
-                    h('span', {
-                        style: {
-                            display: 'inline-block',
-                            float: 'right',
-                            marginRight: '32px'
-                        }
-                    }, [
-                        h('Button', {
-                            props: Object.assign({}, this.buttonProps, {
-                                icon: 'ios-add'
-                            }),
-                            style: {
-                                marginRight: '8px'
-                            },
-                            on: {
-                                click: () => { this.append(data) }
-                            }
-                        }),
-                        h('Button', {
-                            props: Object.assign({}, this.buttonProps, {
-                                icon: 'ios-remove'
-                            }),
-                            on: {
-                                click: () => { this.remove(root, node, data) }
-                            }
-                        })
-                    ])
-                ]);
-            },
-            append (data) {
-                const children = data.children || [];
-                children.push({
-                    title: 'appended node',
-                    expand: true
-                });
-                this.$set(data, 'children', children);
-            },
-            remove (root, node, data) {
-                const parentKey = root.find(el => el === node).parent;
-                const parent = root.find(el => el.nodeKey === parentKey).node;
-                const index = parent.children.indexOf(data);
-                parent.children.splice(index, 1);
+            navigation(node){
+                if(node.length > 0) {
+                    let href = node[0].href.split("#")[1];
+                    window.location.href = "#" + href
+                }
             }
         }
     }
 </script>
 
-<style scoped>
-
+<style>
+    .toc-header {
+        height: 64px;
+        float: start;
+        background: rgb(55, 62, 67);
+    }
+    .toc-back-top {
+        position: absolute;
+        z-index: 999;
+    }
+    .ivu-drawer-body {
+        width: 100%;
+        height: calc(100% - 51px);
+        font-size: 14px;
+        line-height: 1.5;
+        overflow-wrap: break-word;
+        position: absolute;
+        padding: 0px;
+        overflow: auto;
+    }
+    .toc-content-tree {
+        padding: 10px;
+    }
 </style>
--- src/content/index.js
+++ src/content/index.js
@@ -2,39 +2,107 @@ import Vue from "vue/dist/vue.esm.js";
 import 'view-design/dist/styles/iview.css';
 import SideBar from "./components/SideBar"
 
-import {Button, Drawer, Tree} from "view-design";
+import {
+    Button,
+    Drawer,
+    Tree,
+    BackTop
+} from "view-design";
 
 Vue.component("Button", Button);
 Vue.component("Drawer", Drawer);
 Vue.component("Tree", Tree);
+Vue.component("BackTop", BackTop);
 Vue.component('side-bar', SideBar);
 
-(function insertViewIcons() {
-    let ViewIcons = document.createElement('style');
-    ViewIcons.type = 'text/css';
-    ViewIcons.textContent = `
-        @font-face {
-            font-family: "Ionicons";
-            src: url('${ window.chrome.extension.getURL("fonts/ionicons.woff2")}') format('woff'),
-            url('${ window.chrome.extension.getURL("fonts/ionicons.woff")}') format('woff'),
-            url('${ window.chrome.extension.getURL("fonts/ionicons.ttf")}') format('truetype');
+// let githubHeader = document.getElementsByClassName("Header")[0];
+// githubHeader.style['padding-right'] = "300px";
+function extractHeadings() {
+    let headList = [];
+    for(let i = 1; i <=6; i++) {
+        headList.push("h" + i + ">" + "a.anchor");
+    }
+    let headCssSelectors = headList.join();
+    return document.querySelectorAll(headCssSelectors);
+}
+
+function buildTOCTree(heads) {
+    let tree = {
+        title:"github-md-toc",
+        expand: true,
+        children: [],
+        parent: null,
+        tag: "A",
+        href: "",
+    };
+    let pre = tree;
+    for(let i=0; i < heads.length; i++) {
+        let tagName = heads[i].parentNode.tagName;
+        if(tagName <= pre.tag) {
+            while(tagName <= pre.tag) {
+                pre = pre.parent;
+            }
         }
-    `;
-    document.head.appendChild(ViewIcons);
-})();
+        pre.children.push({
+            title: heads[i].parentNode.textContent,
+            expand: true,
+            children: [],
+            parent: pre,
+            tag: tagName,
+            href: heads[i].href
+        });
+        pre = pre.children[pre.children.length - 1];
+    }
+    return tree;
+}
+
+function getComponentData() {
+    let heads = extractHeadings();
+    let data = buildTOCTree(heads);
+    return data
+}
 
 function insertSideBar() {
     let sideBar = document.createElement('div');
     sideBar.id = "toc-side-bar";
-    sideBar.innerHTML = '<side-bar></side-bar>';
-    let mainDom = document.getElementById('js-repo-pjax-container');
+    sideBar.innerHTML = '<side-bar :tree="tree"></side-bar>';
+    let mainDom = document.getElementsByTagName('main')[0];
+    // mainDom.style['padding-right'] = "260px";
     mainDom.appendChild(sideBar);
+    let tree = getComponentData();
     new Vue({
        el: "#toc-side-bar",
+       data: {
+           tree: tree.children
+       }
     });
 }
 
-document.body.onload = insertSideBar;
+let markdown = document.getElementsByTagName("article")[0];
+
+if (markdown) {
+    (function insertViewIcons() {
+        let ViewIcons = document.createElement('style');
+        ViewIcons.type = 'text/css';
+        ViewIcons.textContent = `
+            @font-face {
+                font-family: "Ionicons";
+                src: url('${ window.chrome.extension.getURL("fonts/ionicons.woff2")}') format('woff2'),
+                url('${ window.chrome.extension.getURL("fonts/ionicons.woff")}') format('woff'),
+                url('${ window.chrome.extension.getURL("fonts/ionicons.ttf")}') format('truetype');
+            }
+        `;
+        document.head.appendChild(ViewIcons);
+    })();
+    document.body.onload = insertSideBar;
+}
+
+
+
+
+
+
+
 
 
 
